266d66e56f824cd274af3af5f2260ddb8e4b4f43
abdirahman <abdirahman.ismail@student.moringaschool.com>
2023-10-21 12:45:28 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reverseWord= word.split('').reverse().join('');
+  if (word=== reverseWord) {
+    return true
+  }
+  else {
+    return false
+  }
 }
+console.log(isPalindrome('mom'));
 
 /* 
   Add your pseudocode here
+  reverse the word 
+  check if it is still the same
+  return true if the same 
+  return false if it is different
 */
 
 /*
   Add written explanation of your solution here
+  when reversing you turn the word into array first then reverse and join it
 */
 
 // You can run `node index.js` to view these console logs
